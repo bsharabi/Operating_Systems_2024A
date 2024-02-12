73963a97bbf123e5b5766dca2c1305ccd7bf8a36
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2024-02-12 11:47:36 +0200

    myShell-2

---
+++ Monday/L08/shell/MyShell.c
@@ -0,0 +1,30 @@
+
+#include "MyShell.h"
+#include "MyFunctionShell.h"
+int main()
+{
+    wellcome();
+    getLocation();
+    while (1)
+    {
+        char *str = inputFromUser();
+        puts(str);
+        char ** argumnts = splitString(str);
+        
+        for (int i = 0; argumnts[i]!=NULL; i++)
+        {
+            printf("index-> %d",i);
+            
+            puts(argumnts[i]);
+        
+        }
+        break;
+        
+    }
+
+    return 0;
+}
+void wellcome()
+{
+    puts("Hello MyShell");
+}
