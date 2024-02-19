87ca4010a0a8be7467ae54d151901c63d8e19969
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2024-02-19 09:32:27 +0200

    mt shell

---
+++ Monday/L09/shell/MyFunctionShell.c
@@ -0,0 +1,50 @@
+#include "MyFunctionShell.h"
+
+char *inputFromUser()
+{
+    char ch;
+    int size = 0;
+    char *str = (char *)malloc((size + 1) * sizeof(char));
+    // stdin = hello hello\n
+    while ((ch = getchar()) != '\n')
+    {
+        *(str + size) = ch;
+        size++;
+        str = (char *)realloc(str, size + 1);
+    }
+    *(str + size) = '\0';
+    return str;
+}
+char **splitString(char *str)
+{
+
+    char *subStr;
+    subStr = strtok(str, " ");
+    int argumentSize = 2;
+    int index = 0;
+    char **argumentArray = (char **)malloc(argumentSize * sizeof(char *));
+    *(argumentArray + index) = subStr;
+    while ((subStr = strtok(NULL, " ")) != NULL)
+    {
+        index++;
+        argumentSize++;
+        argumentArray = (char **)realloc(argumentArray, argumentSize * sizeof(char *));
+        *(argumentArray + index) = subStr;
+    }
+    *(argumentArray + index + 1) = NULL;
+
+    return argumentArray;
+}
+
+void getLocation()
+{
+    char location[256];
+    if (getcwd(location, sizeof(location)) == NULL)
+    {
+        puts("Error");
+        return;
+    }
+    printf("\033[0;34m");
+    printf("%s$ ", location);
+    printf("\033[0m");
+}
---
+++ Monday/L09/shell/MyShell.c
@@ -0,0 +1,29 @@
+
+#include "MyShell.h"
+#include "MyFunctionShell.h"
+int main()
+{
+    wellcome();
+    while (1)
+    {
+        getLocation();
+        char *str = inputFromUser();
+        char **argumnts = splitString(str);
+
+        for (int i = 0; argumnts[i] != NULL; i++)
+        {
+            printf("index-> %d", i);
+
+            puts(argumnts[i]);
+        }
+        free(str);
+        free(argumnts);
+        break;
+    }
+
+    return 0;
+}
+void wellcome()
+{
+    puts("Hello MyShell");
+}
